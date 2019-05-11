b43dc824237127a233aec1eda946a6d53d7a8c53
jvasquezaj <jvasquezaj@gmail.com>
2019-05-11 15:33:54 -0700

    Add project files.

---
+++ Project9.2/person.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class Person
+{
+public:
+	Person();
+	Person (string p);
+	void befriend(Person p);
+	void unfriend(Person& p);
+	string getFriendNames();
+private:
+	string name;
+	string friends;
+};
+
+Person::Person() {};
+Person::Person(string p)
+{
+	name = p;
+}
+void Person::befriend(Person p)
+{
+	string newFriend = p.name;
+	friends = newFriend + " " + friends;
+	return;
+}
+
+void Person::unfriend(Person& p)
+{
+	istringstream str(friends);
+	string temp;
+	string newstr;
+	while (str >> temp)
+	{
+		if (temp != p.name)
+		{
+			newstr = newstr + temp;
+		}
+	}
+	friends = newstr;
+	return;
+}
+
+string Person::getFriendNames()
+{
+	string outFriend = friends;
+	return outFriend;
+}
+
+int main()
+{
+	string outputFriend;
+	Person john("john");
+	Person joey("joey");
+	Person sam("sam");
+	john.befriend(joey);
+	john.befriend(sam);
+	joey.befriend(john);
+	outputFriend = john.getFriendNames();
+	cout << outputFriend << endl;
+	outputFriend = joey.getFriendNames();
+	cout << outputFriend << endl;
+	john.unfriend(joey);
+	outputFriend = john.getFriendNames();
+	cout << outputFriend << endl;
+
+
+
+	return 0;
+}
